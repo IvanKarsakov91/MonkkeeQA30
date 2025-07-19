82ef602ccab99b3cdbc6fb9bedd209e2702dc1da
IvanKarsakov <IvanKarsakov91@gmail.com>
2025-07-19 16:41:23 +0300

    al12

--- src/test/java/utils/CustomChromeDriver.java
+++ src/test/java/utils/CustomChromeDriver.java
@@ -12,9 +12,8 @@ public class CustomChromeDriver implements WebDriverProvider {
     public WebDriver createDriver(org.openqa.selenium.Capabilities ignored) {
         ChromeOptions options = new ChromeOptions();
 
-        String uuid = UUID.randomUUID().toString();
-        String profilePath = "/tmp/profile-" + uuid;
-        System.out.println("Запуск Chrome с профилем: " + profilePath);
+        String profilePath = System.getProperty("user.home") + "/.chrome-profile-" + UUID.randomUUID();
+        System.out.println("Chrome profile: " + profilePath);
 
         options.addArguments("--user-data-dir=" + profilePath);
         options.addArguments("--window-size=1280,800");
